0f9f371023668054088928f28ba7f5954a9bdf9b
MariaGiron-code <1202mariag@gmail.com>
2025-05-07 21:52:09 -0500

    -Aplicación de las validaciones

--- ejercicio3/Main.java
+++ ejercicio3/Main.java
@@ -2,7 +2,7 @@ package ejercicio3;
 
 public class Main {
     public static void main(String[] args) {
-        String saludo = saludo.saludarUsuario("Santy");
-        System.out.println(saludo);
+
+
     }
 }
