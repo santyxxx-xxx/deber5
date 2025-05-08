5b5573c3696b1a4fb43825817038118150121ea0
MariaGiron-code <1202mariag@gmail.com>
2025-05-07 20:35:17 -0500

    - Creación de los métodos acelerar y frenar para controlar la velocidad de un auto. - Colocar el atributo velocidad con mificador de acceso privado -Uso de get para acceder al valor de la velocidad y set para las operaciones de velocidad maxima y minima. - Uso de la calse Scanner para que los valores sean ingresados por la consola.

--- ejercicio9/Automoviles.java
+++ ejercicio9/Automoviles.java
@@ -1,7 +1,7 @@
 package ejercicio9;
 
 public class Automoviles {
-    public float velocidad;
+    private float velocidad;
 
     public Automoviles(float velocidad) {
         this.velocidad = velocidad;
@@ -21,5 +21,29 @@ public class Automoviles {
         } else {
             System.out.println("Velocidad valida "+velocidad+" km/h");
         }
+    }
+    // Métodos acelerar y frenar
+
+    public void acelerar(float incrementa) {
+        if (velocidad + incrementa > 200) {
+            System.out.println("No se puede acelerar más allá de 200 km/h.");
+        } else {
+            velocidad += incrementa;
+            System.out.println("Velocidad actual: " + velocidad + " km/h");
+        }
+    }
+    public void frenar(float disminuye) {
+        if (velocidad - disminuye < 0) {
+            System.out.println("No se puede frenar más allá de 0 km/h.");
+        } else {
+            velocidad -= disminuye;
+            System.out.println("Velocidad actual: " + velocidad + " km/h");
+        }
+    }
+    public float getVelocidad() {
+        return velocidad;
+    }
+    public void setVelocidad(float velocidad) {
+        this.velocidad = velocidad;
     }
 }
--- ejercicio9/Main.java
+++ ejercicio9/Main.java
@@ -9,20 +9,28 @@ public class Main {
         System.out.print("Elija una opción (1 para verificar velocidad de aceleracion, 2 para verificar velocidad de frenado): ");
         int opcion = sc.nextInt();
 
-        System.out.print("Ingrese la velocidad: ");
-        float velocidad = sc.nextFloat();
+        System.out.print("Ingrese la velocidad inicial del automóvil: ");
+        float velocidadInicial = sc.nextFloat();
+        Automoviles auto = new Automoviles(velocidadInicial);
 
+        System.out.println("Velocidad inicial: " + auto.getVelocidad() + " km/");
+        // Aceleración
+        System.out.print("Ingrese la cantidad de km/h para acelerar: ");
+        float acelerar = sc.nextFloat();
+        auto.acelerar(acelerar);
 
-        Automoviles validacion = new Automoviles(velocidad);
+        // Validación de velocidad después de acelerar con el metodo
+        auto.validarAceleracion();
 
-        if (opcion == 1) {
-            validacion.validarAceleracion();
-        } else if (opcion == 2) {
-            validacion.validarFrenado();
-        } else {
-            System.out.println("Opción no válida.");
-        }
+        // Frenado
+        System.out.print("Ingrese la cantidad de km/h para frenar: ");
+        float frenar = sc.nextFloat();
+        auto.frenar(frenar);
 
+        // Validación de velocidad después de frenar con el metoso
+        auto.validarFrenado();
+
+        System.out.println("Velocidad final: " + auto.getVelocidad() + " km/h");
         sc.close();
     }
 }
